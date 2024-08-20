305738412ae78a433dfe69130dea44fb2e186d18
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2024-08-20 10:22:01 +0200

    First commit

---
+++ monthly_payment.py
@@ -0,0 +1,14 @@
+
+def calculate_monthly_payment(P, rate, years):
+    r = ((rate / 100) / 12)
+    n = 12 * years
+    A = ((P * r * ((1+r) ** n)) / (((1 + r) ** n) - 1))
+    return A
+
+
+principal = float(input("Lånebelopp: "))
+annual_rate = float(input("Årsränta (%): "))
+years = int(input("Löptid (år): "))
+payment = calculate_monthly_payment(principal, annual_rate, years)
+
+print(f"Din månatliga betalning är: {payment:.2f} kr")
